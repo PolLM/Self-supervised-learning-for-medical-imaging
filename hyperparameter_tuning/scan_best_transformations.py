13308c6e26f763536911d5af34615615775cc30e
polatomic <pol.llopart.1@gmail.com>
2022-03-26 10:47:49 +0100

    supervised part transforms

--- hyperparameter_tuning/scan_best_transformations.py
+++ hyperparameter_tuning/scan_best_transformations.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 import torch
 import torchvision
+import torch.nn as nn
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 
@@ -13,14 +14,14 @@ from PIL import Image
 import os
 import ssl
 import sys 
-import json
 
 PROJECT_PATH =  os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 sys.path.insert(0,PROJECT_PATH)
 
 from self_sup_classes.barlow import BarlowTwins
-from utils.training_utils import train_one_epoch
+from utils.training_utils import train_one_epoch,eval_one_epoch
 from utils.logging_utils import save_checkpoint, save_dict_to_pickle
+from utils.metrics import accuracy
 from augmentations.transform_utils import Transform, GaussianBlur, GaussianNoise, BrightnessModulation, ContrastModulation
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
@@ -93,9 +94,9 @@ transformations.
 
 
 config = {
-    "mode": 'scan_transforms',
+    "mode": 'linear_projector',
     "random_seed": 73,
-    "num_epochs": 1,
+    "num_epochs": 20,
     "batch_size": 128,
     "device": torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu"),
     "barlow_lambda": 5e-3,
@@ -103,7 +104,12 @@ config = {
     "lr": 2e-3,
     "optimizer_weight_decay": 1e-5,
     "transforms_prob": 0.5,
-    "img_res": 224
+    "img_res": 224,
+    "num_classes": 4,
+    "num_epochs_sup": 10,
+    "train_frac": 0.8,
+    "test_frac": 0.1,
+    "val_frac": 0.1,
 }
 
 prob = config["transforms_prob"]
@@ -120,78 +126,208 @@ extra_transforms = {
                 "gaussian_blur_noise"     :[GaussianBlur(p=prob), transforms.ToTensor(), GaussianNoise(p=prob)],
                 "equalize"                :[torchvision.transforms.RandomEqualize(p=prob), transforms.ToTensor()],
                 "rotation"                :[torchvision.transforms.RandomRotation(20), transforms.ToTensor()],
-                "crightness_contrast"     :[transforms.ToTensor(), BrightnessModulation(p=prob), ContrastModulation(p=prob)],
+                "brightness_contrast"     :[transforms.ToTensor(), BrightnessModulation(p=prob), ContrastModulation(p=prob)],
                 "perspective"             :[torchvision.transforms.RandomPerspective(distortion_scale=0.15, p=prob), transforms.ToTensor()],
                 "vertical_flip"           :[torchvision.transforms.RandomVerticalFlip(p=prob), transforms.ToTensor()],
 }
 
-
-for extra_T_name, extra_T in extra_transforms.items():
-    print("%"*40)
-    print(f"Applying transformation:  {extra_T_name}")
-    print("%"*40)
-    
-    #Logging
-    checkpoints_path = os.path.join(PROJECT_PATH, f"runs/transforms/{extra_T_name}_transform")
-    writer = SummaryWriter(checkpoints_path)
-    #Resetting seeds to have the same initial model parameters
-    torch.cuda.empty_cache()
-    random.seed(config["random_seed"])
-    np.random.seed(config["random_seed"])
-    torch.manual_seed(config["random_seed"])
-
-    #Define dataset with the trasnformations
-    transform = transforms.Compose(base_transforms +  extra_T)
-
-    dataset = ImageFolder("F:/Datasets/chest-x-ray/COVID-19_Radiography_Dataset/", transform=Transform(transform, transform))
-
-    loader = torch.utils.data.DataLoader(dataset,
-                                        batch_size=config["batch_size"],
-                                        shuffle=False)
-    
-    #Calling model, optimizer and scheduler
-    back_model = torchvision.models.resnet18(zero_init_residual=True)
-    model = BarlowTwins(config["barlow_lambda"])
-    model.add_backbone( 
-                        backbone =back_model, 
-                        latent_id = -2,
-                        monochanel = True, 
-                        backbone_name='resnet', 
-                        verbose=False)
-
-    model.add_projector(
-                        projector_sizes = [512, 512, 512, 512], 
-                        verbose = False) 
-
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"], weight_decay=config["optimizer_weight_decay"])
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=config["num_epochs"], verbose=True)
-
-    #Self-supervised training loop
-    total_loss = []
-    best_loss = 100000
-    for epoch in range(config["num_epochs"]):     
-        losses = train_one_epoch(loader, model, optimizer, config)
-        scheduler.step()
+if config["mode"] == "scan_transforms":
+    '''
+    #################################################################################################
+    Self Supervised training
+    On This part we iterate through all the extra transformations and train the model in a 
+    self-supervised way (barlow twins). We log the rsults and save the last and best model state dict
+    #################################################################################################
+    '''
+    for extra_T_name, extra_T in extra_transforms.items():
+        print("%"*40)
+        print(f"Applying transformation:  {extra_T_name}")
+        print("%"*40)
         
-        mean_epoch_loss = np.mean(losses)
-        total_loss.append(mean_epoch_loss)
-        writer.add_scalar('Loss/train', mean_epoch_loss, epoch)
+        #Logging
+        checkpoints_path = os.path.join(PROJECT_PATH, f"runs/transforms/{extra_T_name}_transform")
+        writer = SummaryWriter(checkpoints_path)
+        #Resetting seeds to have the same initial model parameters
+        torch.cuda.empty_cache()
+        random.seed(config["random_seed"])
+        np.random.seed(config["random_seed"])
+        torch.manual_seed(config["random_seed"])
 
-        if best_loss > mean_epoch_loss:
-            best_loss = mean_epoch_loss
-            save_checkpoint(model.state_dict(), checkpoints_path, f"resnet18_best_state_dict.pt")
+        #Define dataset with the trasnformations
+        transform = transforms.Compose(base_transforms +  extra_T)
 
-        print(f"---> Avg epoch loss: {mean_epoch_loss}" )
+        dataset = ImageFolder("F:/Datasets/chest-x-ray/COVID-19_Radiography_Dataset/", transform=Transform(transform, transform))
 
-    save_dict_to_pickle(config, checkpoints_path)
-    save_checkpoint(model.state_dict(), checkpoints_path, f"resnet18_final_state_dict.pt")
-    save_checkpoint(torch.tensor(total_loss), checkpoints_path, f"epoch_losses.pt")
+        loader = torch.utils.data.DataLoader(dataset,
+                                            batch_size=config["batch_size"],
+                                            shuffle=True)
+        
+        #Calling model, optimizer and scheduler
+        back_model = torchvision.models.resnet18(zero_init_residual=True)
+        model = BarlowTwins(config["barlow_lambda"])
+        model.add_backbone( 
+                            backbone =back_model, 
+                            latent_id = -2,
+                            monochanel = True, 
+                            backbone_name='resnet', 
+                            verbose=False)
 
-    writer.flush()
-    writer.close()
+        model.add_projector(
+                            projector_sizes = [512, 512, 512, 512], 
+                            verbose = False) 
 
-    #Supervised training loop
+
+        optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"], weight_decay=config["optimizer_weight_decay"])
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=config["num_epochs"], verbose=True)
+
+        #Self-supervised training loop
+        total_loss = []
+        best_loss = 100000
+        for epoch in range(config["num_epochs"]):     
+            losses = train_one_epoch(loader, model, optimizer, config)
+            scheduler.step()
+            
+            mean_epoch_loss = np.mean(losses)
+            total_loss += losses
+            writer.add_scalar('Loss/train', mean_epoch_loss, epoch)
+            
+            if best_loss > mean_epoch_loss:
+                best_loss = mean_epoch_loss
+                save_checkpoint(model.state_dict(), checkpoints_path, f"resnet18_best_state_dict.pt")
+
+            print(f"---> Avg epoch loss: {mean_epoch_loss}" )
+
+        save_dict_to_pickle(config, checkpoints_path)
+        save_checkpoint(model.state_dict(), checkpoints_path, f"resnet18_final_state_dict.pt")
+        save_checkpoint(torch.tensor(total_loss), checkpoints_path, f"epoch_losses.pt")
+
+        writer.flush()
+        writer.close()
+
+elif config["mode"] == "linear_projector":
+    '''
+    #################################################################################################
+    Supervised training
+    On This part we iterate through all the extra transformations, we load the self-sup trained models,
+    we freeze its weights, and train a linear predictor on top of it.
+    #################################################################################################
+    '''
+    for extra_T_name, extra_T in extra_transforms.items():
+        print("%"*40)
+        print(f"Applying transformation:  {extra_T_name}")
+        print("%"*40)
+        
+        #Logging
+        selfsup_checkpoints_path = os.path.join(PROJECT_PATH, f"runs/transforms/{extra_T_name}_transform")
+        checkpoints_path = os.path.join(PROJECT_PATH, f"runs/transforms/{extra_T_name}_linear_prediction")
+        writer = SummaryWriter(checkpoints_path)
+        #Resetting seeds to have the same initial model parameters
+        torch.cuda.empty_cache()
+        random.seed(config["random_seed"])
+        np.random.seed(config["random_seed"])
+        torch.manual_seed(config["random_seed"])
+
+        #Define dataset with the trasnformations
+        transform = transforms.Compose(base_transforms +  extra_T)
+
+        dataset = ImageFolder("F:/Datasets/chest-x-ray/COVID-19_Radiography_Dataset/", transform)
+
+        #Test train split
+        train_len = int(config["train_frac"]*len(dataset))
+        test_len = int(config["test_frac"]*len(dataset))
+        val_len = len(dataset) - train_len - test_len
+        train_dataset, test_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_len, test_len, val_len])
+
+        train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=config["batch_size"], 
+        shuffle=True)
+
+        test_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=config["batch_size"], 
+        shuffle=True)
+
+        val_loader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=config["batch_size"], 
+        shuffle=True)
+
+        #Calling resnet model
+        model = torchvision.models.resnet18(zero_init_residual=True)
+        model.conv1 = nn.Conv2d(1, 64, 7, 2, 3, bias=False)
+
+        #loading state dict and adapting it for the model (from Barlow Twins model to simple resnet model)
+        barlow_state_dict = torch.load(os.path.join(selfsup_checkpoints_path, "resnet18_best_state_dict.pt"))
+        state_dict = barlow_state_dict.copy()
+
+        for k,v in barlow_state_dict.items():
+            if "backbone" not in k:
+                del state_dict[k]
+            else:
+                state_dict[k[13:]] = state_dict.pop(k)
+
+        model.load_state_dict(state_dict)
+
+        #Adapt model and add linear projector
+        model.fc = nn.Sequential(nn.Linear(512, config["num_classes"]))
+        
+        #Freeze all parameters but the last one
+        for param in model.parameters():
+            param.requires_grad = False
+        model.fc.requires_grad = True
+
+        #Criterion
+        criterion = torch.nn.CrossEntropyLoss()
+
+        #optimizer and scheduler
+        optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"], weight_decay=config["optimizer_weight_decay"])
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=config["num_epochs"], verbose=True)
+
+        #Supervised training loop
+        total_loss = []
+        total_loss_eval = []
+        total_acc = []
+        total_acc_eval = []
+        best_loss = 100000
+        for epoch in range(config["num_epochs"]):    
+
+            losses, targets, predictions = train_one_epoch(train_loader, model, optimizer, config, criterion=criterion, return_targets=True)
+            losses_eval, targets_eval, predictions_eval = eval_one_epoch(train_loader, model, optimizer, config, criterion=criterion, return_targets=True)
+            scheduler.step()
+
+            acc = accuracy(targets, predictions)
+            acc_eval = accuracy(targets_eval, predictions_eval)
+            
+            mean_epoch_loss = np.mean(losses)
+            mean_epoch_loss_eval = np.mean(losses_eval)
+
+            total_loss += losses
+            total_loss_eval += losses_eval
+            total_acc.append(acc)
+            total_acc_eval.append(acc)
+
+            writer.add_scalar('Loss/train', mean_epoch_loss, epoch)
+            writer.add_scalar('Loss/eval', mean_epoch_loss_eval, epoch)
+
+            writer.add_scalar('Acc/train', acc, epoch)
+            writer.add_scalar('Acc/eval', acc_eval, epoch)
+            
+            if best_loss > mean_epoch_loss_eval:
+                best_loss = mean_epoch_loss_eval
+                save_checkpoint(model.state_dict(), checkpoints_path, f"resnet18_best_state_dict.pt")
+
+            print(f"---> Avg epoch loss: {mean_epoch_loss}, avg epoch loss eval: {mean_epoch_loss_eval}" )
+
+        save_dict_to_pickle(config, checkpoints_path)
+        save_checkpoint(model.state_dict(), checkpoints_path, f"resnet18_final_state_dict.pt")
+        save_checkpoint(torch.tensor(total_loss), checkpoints_path, f"epoch_losses.pt")
+        save_checkpoint(torch.tensor(total_loss_eval), checkpoints_path, f"epoch_losses_eval.pt")
+        save_checkpoint(torch.tensor(total_acc), checkpoints_path, f"epoch_acc.pt")
+        save_checkpoint(torch.tensor(total_acc_eval), checkpoints_path, f"epoch_acc_eval.pt")
+
+        writer.flush()
+        writer.close()
 
 
 # %%
