4094cb1d94597e6e27303afd791d96a88dc00836
FrancescGP <98765584+FrancescGP@users.noreply.github.com>
2022-03-27 18:40:53 +0200

    Add helper function to store and load model, optimizer, scheduler states and hyperparameters.

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -4,6 +4,25 @@ import json
 import torch
 import yaml
 import pickle
+import time
+
+def get_file_name():
+    return time.asctime()+'_checkpoint.pth.tar'
+
+def store_data(model, optimizer, scheduler, hyperparams):
+    checkpoint = {}
+    checkpoint['model'] = model.state_dict()
+    checkpoint['optimizer'] = optimizer.state_dict()
+    checkpoint['scheduelr'] = scheduler.state_dict()
+    checkpoint['hyperparams'] = hyperparams
+    save_checkpoint(checkpoint, False, get_file_name())
+
+def load_data(model, optimizer, scheduler, filename):
+    checkpoint = torch.load(filename)
+    model.load_state(checkpoint['model'])
+    optimizer.load_state(checkpoint['optimizer'])
+    scheduler.load_state(checkpoint['scheduler'])
+    return checkpoint['hyperparams']
 
 #code from: https://stackoverflow.com/questions/7100125/storing-python-dictionaries
 def save_dict_to_pickle(state, basepath = os.getcwd(), filename='config.p'):
