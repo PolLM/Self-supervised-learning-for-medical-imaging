51598ea1cbf2522d4c33d6d501a04d267d2632f7
FrancescGP <98765584+FrancescGP@users.noreply.github.com>
2022-04-03 19:11:49 +0200

    Update logging_utils.py
    Add a function that converts from one Tensorboard working directory to a pandas variable

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -6,6 +6,11 @@ import yaml
 import pickle
 import time
 
+import traceback
+import pandas as pd
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+
+
 def get_file_name():
     return time.asctime()+'_checkpoint.pth.tar'
 
@@ -54,3 +59,23 @@ def save_config_file(model_checkpoints_folder, args):
 
 
 
+
+# Extract csv data from a Tensorboard directory
+def tflog2pandas(path):
+    runlog_data = pd.DataFrame({"metric": [], "value": [], "step": []})
+    try:
+        event_acc = EventAccumulator(path)
+        event_acc.Reload()
+        tags = event_acc.Tags()["scalars"]
+        for tag in tags:
+            event_list = event_acc.Scalars(tag)
+            values = list(map(lambda x: x.value, event_list))
+            step = list(map(lambda x: x.step, event_list))
+            r = {"metric": [tag] * len(step), "value": values, "step": step}
+            r = pd.DataFrame(r)
+            runlog_data = pd.concat([runlog_data, r])
+    # Dirty catch of DataLossError
+    except Exception:
+        print("Event file possibly corrupt: {}".format(path))
+        traceback.print_exc()
+    return runlog_data
