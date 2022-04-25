8dea1810391882eef6473c25aab922b921a1f691
FrancescGP <98765584+FrancescGP@users.noreply.github.com>
2022-04-25 07:50:01 +0200

    Update trained_vs_no_trained_compare.py

--- downstream_task/trained_vs_no_trained_compare.py
+++ downstream_task/trained_vs_no_trained_compare.py
@@ -1,8 +1,8 @@
-model_dict = "/content/resnet18_300epoc_self_supervised/resnet18_best_state_dict.pt"
-#batch_size = 128
+model_dict = [...] # Path to the file containing the state dict to be used for the analysis.
 batch_size = 128
 num_classes = 4
 
+# Modify these two variables to contain all the labels to be scanned.
 ListOfCompares = [0.01,0.05,0.10,0.15,0.20,0.25]
 ListOfTags=      ["1 percent of samples",
                     "5 percent of samples",
@@ -17,8 +17,8 @@ ListOfDicts_resnet_1 = [None,None,None,None,None,None]
 ListOfDicts_resnet = [None,None,None,None,None,None]
 
 config = {
-    'project_path': 'D:\\Documents\\GitHub\\aidl2022_final_project', 
-    'model_path': 'D:\\Documents\\GitHub\\aidl2022_final_project\\runs\\final_trainings\\0.005_512__512__300', 
+    'project_path': [...] # Update with the base path of your project.
+    'model_path': [...] # Update with the base path of your project.
     'random_seed': 73, 
     'device': torch.device(type='cuda'), 
     'img_res': 224, 
@@ -53,12 +53,9 @@ train_len = int(len(dataset)*config["train_frac"])
 test_len = int(len(dataset)-val_len - train_len)
 train_dataset, val_dataset,test_dataset = torch.utils.data.random_split(dataset, [train_len, val_len,test_len])
 
-writer = SummaryWriter(log_dir="/content/Compare_Dataset/",comment="Compare trained vs untrained without logsoftmax")
+# Update with your log directory
+writer = SummaryWriter(log_dir="[...]",comment="Compare trained vs untrained without logsoftmax")
 
-# Train supervised: 25 epochs 
-# provar 3 epochs només amb fc. 
-# Tot descongelat 
-# Dataset covid 
 print(f'Total samples: {len(train_dataset)} as Training dataset')
 print(f'Total samples: {len(val_dataset)} as Validation dataset')
 for idx,perc in enumerate(ListOfCompares):
@@ -73,34 +70,36 @@ for idx,perc in enumerate(ListOfCompares):
     comparison.train(dataset_reduced, val_dataset, config['num_epochs_sup'],criterion,config,writer,tag)    
     ListOfDicts_barlow[idx], ListOfDicts_resnet[idx], ListOfDicts_barlow_1[idx], ListOfDicts_resnet_1[idx] = comparison.get_dicts()
 
-save_checkpoint(ListOfDicts_barlow[0],basepath='/content/state_dicts',filename='Barlow_15_epocs_1perc.pt')
-save_checkpoint(ListOfDicts_barlow[1],basepath='/content/state_dicts',filename='Barlow_15_epocs_5perc.pt')
-save_checkpoint(ListOfDicts_barlow[2],basepath='/content/state_dicts',filename='Barlow_15_epocs_10perc.pt')
-save_checkpoint(ListOfDicts_barlow[3],basepath='/content/state_dicts',filename='Barlow_15_epocs_15perc.pt')
-save_checkpoint(ListOfDicts_barlow[4],basepath='/content/state_dicts',filename='Barlow_15_epocs_20perc.pt')
-save_checkpoint(ListOfDicts_barlow[5],basepath='/content/state_dicts',filename='Barlow_15_epocs_25perc.pt')
+base_path = [...] # Update with the path to save the state dict.
+
+save_checkpoint(ListOfDicts_barlow[0],basepath=base_path,filename='Barlow_15_epocs_1perc.pt')
+save_checkpoint(ListOfDicts_barlow[1],basepath=base_path,filename='Barlow_15_epocs_5perc.pt')
+save_checkpoint(ListOfDicts_barlow[2],basepath=base_path,filename='Barlow_15_epocs_10perc.pt')
+save_checkpoint(ListOfDicts_barlow[3],basepath=base_path,filename='Barlow_15_epocs_15perc.pt')
+save_checkpoint(ListOfDicts_barlow[4],basepath=base_path,filename='Barlow_15_epocs_20perc.pt')
+save_checkpoint(ListOfDicts_barlow[5],basepath=base_path,filename='Barlow_15_epocs_25perc.pt')
 
 
-save_checkpoint(ListOfDicts_barlow_1[0],basepath='/content/state_dicts',filename='Barlow_1_epocs_1perc.pt')
-save_checkpoint(ListOfDicts_barlow_1[1],basepath='/content/state_dicts',filename='Barlow_1_epocs_5perc.pt')
-save_checkpoint(ListOfDicts_barlow_1[2],basepath='/content/state_dicts',filename='Barlow_1_epocs_10perc.pt')
-save_checkpoint(ListOfDicts_barlow_1[3],basepath='/content/state_dicts',filename='Barlow_1_epocs_15perc.pt')
-save_checkpoint(ListOfDicts_barlow_1[4],basepath='/content/state_dicts',filename='Barlow_1_epocs_20perc.pt')
-save_checkpoint(ListOfDicts_barlow_1[5],basepath='/content/state_dicts',filename='Barlow_1_epocs_25perc.pt')
+save_checkpoint(ListOfDicts_barlow_1[0],basepath=base_path,filename='Barlow_1_epocs_1perc.pt')
+save_checkpoint(ListOfDicts_barlow_1[1],basepath=base_path,filename='Barlow_1_epocs_5perc.pt')
+save_checkpoint(ListOfDicts_barlow_1[2],basepath=base_path,filename='Barlow_1_epocs_10perc.pt')
+save_checkpoint(ListOfDicts_barlow_1[3],basepath=base_path,filename='Barlow_1_epocs_15perc.pt')
+save_checkpoint(ListOfDicts_barlow_1[4],basepath=base_path,filename='Barlow_1_epocs_20perc.pt')
+save_checkpoint(ListOfDicts_barlow_1[5],basepath=base_path,filename='Barlow_1_epocs_25perc.pt')
 
-save_checkpoint(ListOfDicts_resnet[0],basepath='/content/state_dicts',filename='resnet_15_epocs_1perc.pt')
-save_checkpoint(ListOfDicts_resnet[1],basepath='/content/state_dicts',filename='resnet_15_epocs_5perc.pt')
-save_checkpoint(ListOfDicts_resnet[2],basepath='/content/state_dicts',filename='resnet_15_epocs_10perc.pt')
-save_checkpoint(ListOfDicts_resnet[3],basepath='/content/state_dicts',filename='resnet_15_epocs_15perc.pt')
-save_checkpoint(ListOfDicts_resnet[4],basepath='/content/state_dicts',filename='resnet_15_epocs_20perc.pt')
-save_checkpoint(ListOfDicts_resnet[5],basepath='/content/state_dicts',filename='resnet_15_epocs_25perc.pt')
+save_checkpoint(ListOfDicts_resnet[0],basepath=base_path,filename='resnet_15_epocs_1perc.pt')
+save_checkpoint(ListOfDicts_resnet[1],basepath=base_path,filename='resnet_15_epocs_5perc.pt')
+save_checkpoint(ListOfDicts_resnet[2],basepath=base_path,filename='resnet_15_epocs_10perc.pt')
+save_checkpoint(ListOfDicts_resnet[3],basepath=base_path,filename='resnet_15_epocs_15perc.pt')
+save_checkpoint(ListOfDicts_resnet[4],basepath=base_path,filename='resnet_15_epocs_20perc.pt')
+save_checkpoint(ListOfDicts_resnet[5],basepath=base_path,filename='resnet_15_epocs_25perc.pt')
 
-save_checkpoint(ListOfDicts_resnet_1[0],basepath='/content/state_dicts',filename='resnet_1_epocs_1perc.pt')
-save_checkpoint(ListOfDicts_resnet_1[1],basepath='/content/state_dicts',filename='resnet_1_epocs_5perc.pt')
-save_checkpoint(ListOfDicts_resnet_1[2],basepath='/content/state_dicts',filename='resnet_1_epocs_10perc.pt')
-save_checkpoint(ListOfDicts_resnet_1[3],basepath='/content/state_dicts',filename='resnet_1_epocs_15perc.pt')
-save_checkpoint(ListOfDicts_resnet_1[4],basepath='/content/state_dicts',filename='resnet_1_epocs_20perc.pt')
-save_checkpoint(ListOfDicts_resnet_1[5],basepath='/content/state_dicts',filename='resnet_1_epocs_25perc.pt')
+save_checkpoint(ListOfDicts_resnet_1[0],basepath=base_path,filename='resnet_1_epocs_1perc.pt')
+save_checkpoint(ListOfDicts_resnet_1[1],basepath=base_path,filename='resnet_1_epocs_5perc.pt')
+save_checkpoint(ListOfDicts_resnet_1[2],basepath=base_path,filename='resnet_1_epocs_10perc.pt')
+save_checkpoint(ListOfDicts_resnet_1[3],basepath=base_path,filename='resnet_1_epocs_15perc.pt')
+save_checkpoint(ListOfDicts_resnet_1[4],basepath=base_path,filename='resnet_1_epocs_20perc.pt')
+save_checkpoint(ListOfDicts_resnet_1[5],basepath=base_path,filename='resnet_1_epocs_25perc.pt')
 
 
 writer.flush()
